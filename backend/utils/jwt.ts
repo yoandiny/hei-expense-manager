86918013af0036164f4d228f345832088eebb5be
Rindra <hei.rindra.2@gmail.com>
2025-09-07 22:23:14 +0300

    feat: nothing change

--- backend/utils/jwt.ts
+++
@@ -1,9 +0,0 @@
-import * as jwt from "jsonwebtoken"
-
-export function signToken(userId: string): string{
-    return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "1d"});
-}
-
-export function verifyToken(token: string): any{
-    return jwt.verify(token, process.env.JWT_SECRET);
-}
