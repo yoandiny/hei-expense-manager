f237a9c8be48f757e455a2fe483491043446df59
Roxann <rofaniry@gmail.com>
2025-09-08 11:36:26 +0300

    combine all branches in feauture/combined

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { PrismaClient } from '../generated/prisma/client.js'
+import { PrismaClient } from '../generated/prisma/.js'
 
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/user.js"
+import categoryRoutes from "./routes/categories";
+import summaryRoutes from "./routes/summary";
 
 dotenv.config();
 const app = express()
@@ -15,8 +17,10 @@ app.use(express.json())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
+app.use('/api/categories', categoryRoutes);
+app.use('/api/summary', summaryRoutes);
 
-const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
     res.send("Backend Expense Tracker API is running!")
 })
@@ -27,6 +31,12 @@ app.get("/users", async (req, res) => {
     res.json(users);
 });
 
+
+app.get('/', (req, res) => {
+    res.send('API Personal Expense Tracker fonctionne !');
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
     console.log(`✅ Server running on http://localhost:${PORT}`);
