a7e69c816018b45f7b9ce8d6fa0e86b231dcc35f
Yoan <yoandiny14@gmail.com>
2025-09-08 22:35:01 +0300

    Update Signup component layout for improved styling and alignment

--- front/src/pages/auth/Signup.tsx
+++ front/src/pages/auth/Signup.tsx
@@ -61,15 +61,15 @@ const Signup = () => {
     }
   return (
     <div className="flex flex-col items-center mt-1">
-        <img className='w-1/4 items-center' src={Logo} alt="" />
+        <img className='w-1/7 items-center' src={Logo} alt="" />
       <h1 className="text-4xl font-bold mb-10">Inscription</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col w-1/2">
+      <form onSubmit={handleSubmit} className="flex flex-col w-1/2 items-center">
         <label htmlFor="email" className="mb-2">Email :</label>
-        <input type="email" id="email" onChange={handleChange} name="email" className="border-2 p-2 mb-4" /><br />
+        <input type="email" id="email" onChange={handleChange} name="email" className="border-2 p-2 mb-4 w-100 " /><br />
         <label htmlFor="password" className="mb-2">Mot de passe :</label>
-        <input type="password" id="password" onChange={handleChange} name="password" className="border-2 p-2 mb-4" /><br />
+        <input type="password" id="password" onChange={handleChange} name="password" className="border-2 p-2 mb-4 w-100" /><br />
         <label htmlFor="repeatPassword" className='mb-2'>Répétez votre mot de passe :</label>
-        <input type="password" id="repeatPassword" onChange={handleChange} name="confirmationPassword" className="border-2 p-2 mb-4" /><br />
+        <input type="password" id="repeatPassword" onChange={handleChange} name="confirmationPassword" className="w-100 border-2 p-2 mb-4" /><br />
         <p>Vous avez deja un compte ? <Link to="/login" className="text-blue-500">Connectez-vous</Link></p>
         <input type="submit" value="S'inscrire"  className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
       </form>
