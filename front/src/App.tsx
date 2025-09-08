7b4c0cfdf2b25e92ebfe1a994a7e310b80cfc5ef
Rindra <hei.rindra.2@gmail.com>
2025-09-08 19:52:42 +0300

    feat : delete div parent of component summary and category

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,13 +5,8 @@ import Summary from "./pages/dashboard/Dashboard";
 function App() {
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-6 gap-12">
-            <div className="w-full max-w-4xl">
                 <Summary />
-            </div>
-
-            <div className="w-full max-w-4xl">
                 <Category />
-            </div>
         </div>
     )
 }
