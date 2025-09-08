c672d1f406c47394e3fa5199f68b9cc4fe529293
Rindra <hei.rindra.2@gmail.com>
2025-09-08 10:37:18 +0300

    feat: add comparePassword and handle message error

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,41 +1,55 @@
 import {Request, Response} from "express";
 import prisma from "../PrismaClient.js";
-import {hashPassword} from "../utils/password.js";
-import {signToken} from "../utils/jwt.js";
+import {comparePassword, hashPassword} from "../utils/password.js";
+import {signToken} from "../utils/jwt.js"
 
 export async function signup(req: Request, res: Response){
     const { email, password } = req.body;
+    try {
+        const existing = await prisma.user.findUnique({where: {email}})
+        if(existing) return res.status(400).json({message: "Email already in use"})
 
-    const existing = await prisma.user.findUnique({where: {email}})
-    if(existing) return res.status(400).json({message: "User already exists"})
+        const hashed = await hashPassword(password);
+        const newUser = await prisma.user.create({
+            data: {email, password: hashed},
+        })
 
-    const hashed = await hashPassword(password);
+        const token = signToken(newUser.id);
+        res.status(201).json({token, user: {id:newUser.id, email:newUser.email}})
+    }catch (error: any){
+        console.error(error)
+        res.status(500).json({error: error.message ||  "Internal server error"})
+    }
 
-    const newUser = await prisma.user.create({
-        data: {email, password: hashed},
-    })
-
-    res.json({message: "User created successfully", id: newUser.id})
 }
 
 export async function login(req: Request, res: Response){
     const { email, password } = req.body;
+    try {
+        const user= await prisma.user.findUnique({where: {email}})
+        if(!user) return res.status(401).json({message: "User not found"})
 
-    const user= await prisma.user.findUnique({where: {email}})
-    if(!user) return res.status(400).json({message: "User not found"})
+        const isValid = await comparePassword(password, user.password)
+        if(!isValid) return res.status(401).json({message: "Invalid password"})
+        const token = signToken(user.id);
 
-    const token = signToken(user.id);
-
-    res.json({token});
+        res.json({token, user: {id: user.id, email: user.email}});
+    } catch (error: any) {
+        res.status(500).json({message: error.message || "Invalid login"})
+    }
 }
 
 export async function me(req: Request, res: Response){
     const userId = (req as any).user;
 
-    const user = await prisma.user.findUnique({
-        where: {id: userId}
-    })
-    if(!user) return res.status(400).json({message: "User not found"})
+    try {
+        const user = await prisma.user.findUnique({
+            where: {id: userId}
+        })
+        if(!user) return res.status(404).json({message: "User not found"})
 
-    res.json({id: user.id, email: user.email})
+        res.json({id: user.id, email: user.email})
+    }catch (error: any){
+        res.status(500).json({message: error.message ||"Internal server error"})
+    }
 }
