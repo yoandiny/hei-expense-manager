668f975c3d41bb2089da1f96a716339a24540018
Rindra <hei.rindra.2@gmail.com>
2025-09-08 10:38:04 +0300

    feat: add path "/api/user"

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,21 @@
 import express from "express";
-import * as dotenv from "dotenv";
+import cors from "cors";
+import dotenv from "dotenv";
 import { PrismaClient } from '../generated/prisma/client.js'
+
 import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/user.js"
 
 dotenv.config();
-
 const app = express()
 const prisma = new PrismaClient()
 
-app.use("/api/auth", authRoutes)
+app.use(cors());
 app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
+
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Backend Expense Tracker API is running!")
