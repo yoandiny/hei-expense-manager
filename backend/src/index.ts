458a3826e1d9551cab759bdec15726f968cb9d0c
Roxann <rofaniry@gmail.com>
2025-09-08 11:52:33 +0300

    Save Changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { PrismaClient } from '../generated/prisma/.js'
+import { PrismaClient } from "@prisma/client";
 
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/user.js"
