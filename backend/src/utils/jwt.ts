f4a02ef835724578751a253c95471f71ac720c6d
Rindra <hei.rindra.2@gmail.com>
2025-09-08 20:31:43 +0300

    feat : reorder the emplacement

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
-dotenv.config();
 import jwt from "jsonwebtoken";
+
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) throw new Error("JWT_SECRET is not defined in .env");
