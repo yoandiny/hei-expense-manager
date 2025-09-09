1dd33f76c8b6158c1915519eedfdeaf23d2ebd1d
Yoan <yoandiny14@gmail.com>
2025-09-09 10:22:13 +0300

    refactor: update Navbar layout and remove Sidebar component

--- front/src/components/layout/Navbar.tsx
+++ front/src/components/layout/Navbar.tsx
@@ -1,126 +1,70 @@
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { Link, Outlet } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <nav className="bg-gray-800">
+    <div className='flex flex-col'>
+    <div>
+      <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-23">
           <div className="flex items-center">
             <div className="flex-shrink-0">
               <Link to="/">
                 <img
-                  className="h-50 w-50"
+                  className="h-30 w-30"
                   src={Logo}
                   alt="Logo"
                 />
               </Link>
             </div>
             <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                <Link
+              
+            </div>
+          </div>
+          <div className="hidden md:block">
+          </div>
+          <div className="-mr-2 flex md:hidden">
+           
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div className='flex '>
+      <nav className='w-100 h-full mr-10'>
+      <div className="bg-gray-100 p-4 h-150 flex flex-col mb-1">
+        <Link
                   to="/dashboard"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-large"
                 >
                   Tableau de bord
                 </Link>
 
                 <Link
                   to="/categories"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-black-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-large"
                 >
                   Catégories
                 </Link>
 
                 <Link
                   to="/transactions"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-black-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Transactions
                 </Link>
-              </div>
-            </div>
-          </div>
-          <div className="hidden md:block">
-            <div className="ml-4 flex items-center md:ml-6">
-              <button
-                className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-haspopup="true"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className="-mr-2 flex md:hidden">
-            <button
-              className="bg-gray-800 p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              aria-haspopup="true"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
       </div>
 
-      <div className={isOpen ? 'block' : 'hidden'}>
-        <div className="px-2 pt-2 pb-3 space-y-1">
-          <Link
-            to="/dashboard"
-            className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900"
-          >
-            Tableau de bord
-          </Link>
+      </nav>
+      <main>
+      <Outlet />
+    </main>
+    </div>
+    
 
-          <Link
-            to="/categories"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Catégories
-          </Link>
-
-          <Link
-            to="/transactions"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Transactions
-          </Link>
-        </div>
-      </div>
-    </nav>
+    </div>
+    
+    </div>
   )
 }
