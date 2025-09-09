d4c585d5b0d3695f9d795aae71557586d9d6eb1f
Yoan <yoandiny14@gmail.com>
2025-09-09 09:35:25 +0300

    Enhance Signup component styling with shadow and rounded corners for improved UI

--- front/src/pages/auth/Signup.tsx
+++ front/src/pages/auth/Signup.tsx
@@ -63,7 +63,7 @@ const Signup = () => {
     <div className="flex flex-col items-center mt-1">
         <img className='w-1/7 items-center' src={Logo} alt="" />
       <h1 className="text-4xl font-bold mb-10">Inscription</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col w-1/2 items-center">
+      <form onSubmit={handleSubmit} className="flex flex-col w-1/2 items-center shadow-2xl rounded-2xl">
         <label htmlFor="email" className="mb-2">Email :</label>
         <input type="email" id="email" onChange={handleChange} name="email" className="border-2 p-2 mb-4 w-100 " /><br />
         <label htmlFor="password" className="mb-2">Mot de passe :</label>
@@ -71,7 +71,7 @@ const Signup = () => {
         <label htmlFor="repeatPassword" className='mb-2'>Répétez votre mot de passe :</label>
         <input type="password" id="repeatPassword" onChange={handleChange} name="confirmationPassword" className="w-100 border-2 p-2 mb-4" /><br />
         <p>Vous avez deja un compte ? <Link to="/login" className="text-blue-500">Connectez-vous</Link></p>
-        <input type="submit" value="S'inscrire"  className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+        <input type="submit" value="S'inscrire"  className=" mb-1 cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
       </form>
       <ToastContainer />
     </div>
