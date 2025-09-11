2ceaa9fab4389366bc43bb9ad4326ccac67a9071
Yoan <yoandiny14@gmail.com>
2025-09-11 10:10:58 +0300

    feat: update Vite configuration to load environment variables and use dynamic API URL

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default ({mode}:any) =>{
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+
+  return defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
+}
