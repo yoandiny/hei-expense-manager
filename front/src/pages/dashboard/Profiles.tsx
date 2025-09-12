90cbf75f14f068762c0432255ae02a9d7c494ed8
Yoan <yoandiny14@gmail.com>
2025-09-12 22:40:45 +0300

    feat: added dark mode style on profile and navbar

--- front/src/pages/dashboard/Profiles.tsx
+++ front/src/pages/dashboard/Profiles.tsx
@@ -60,45 +60,46 @@ useEffect(()=>{
     getProfile();
 })
     return (
-        <div className="min-h-screen p-8 bg-gray-50 flex justify-center items-start">
-            <div className="w-full max-w-2xl bg-white p-8 rounded-2xl shadow-lg">
+        <div className="min-h-screen p-8 bg-gray-50  flex justify-center items-start">
+            <div className="w-full max-w-2xl bg-white dark:bg-slate-700 p-8 rounded-2xl shadow-lg">
                 <section className="text-center mb-10">
-                    <h2 className="text-3xl font-bold text-green-800 mb-4">My Profile</h2>
+                    <h2 className="text-3xl font-bold text-green-800 dark:text-slate-100 dark:border-slate-600 mb-4">My Profile</h2>
                     <img src={ProfilePic} alt="Profile picture" className="w-32 h-32 rounded-full mx-auto border-4 border-gray-200 shadow"/>
                 </section>
                 <section className="mb-8">
-                    <h3 className="text-xl font-semibold text-green-800 mb-4">Personal Information</h3>
+                    <h3 className="text-xl font-semibold text-green-800 dark:text-slate-400 mb-4">Personal Information</h3>
                     {isEditing ? (
                         <div className="space-y-6">
                             <div className="flex flex-col">
-                                <label className="text-gray-700 mb-1">Email Address</label>
+                                <label className="text-gray-700 dark:text-slate-400 mb-1">Email Address</label>
                                 <input type="email" name="email" value={profileData.email} onChange={handleChange} placeholder={user.email} className="border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-green-400 outline-none"/>
                             </div>
+                            
 
                             <div className="flex flex-col">
-                                <label className="text-gray-700 mb-1">New Password</label>
+                                <label className="text-gray-700 dark:text-slate-400 mb-1">New Password</label>
                                 <input type="password" name="password" value={profileData.password} onChange={handleChange} placeholder="********" className="border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-green-400 outline-none"/>
                             </div>
                         </div>
                     ) : (
                         <div className="space-y-4">
                             <div className="flex justify-between border-b border-gray-200 pb-3">
-                                <span className="text-gray-700">Email Address</span>
+                                <span className="text-gray-700 dark:text-slate-400">Email Address</span>
                                 <span className="font-semibold text-gray-800">{user.email}</span>
                             </div>
 
                             <div className="flex justify-between border-b border-gray-200 pb-3">
-                                <span className="text-gray-700">Password</span>
+                                <span className="text-gray-700 dark:text-slate-400">Password</span>
                                 <span className="font-semibold text-gray-800">**********</span>
                             </div>
                         </div>
                     )}
                 </section>
                 <section className="mb-8">
-                    <h3 className="text-xl font-semibold text-green-800 mb-4">Features</h3>
+                    <h3 className="text-xl font-semibold text-green-800 dark:text-slate-400 mb-4">Features</h3>
 
                     <div className="flex justify-between items-center border-b border-gray-200 pb-3">
-                        <span className="text-gray-700">Enable Dark Mode</span>
+                        <span className="text-gray-700 dark:text-slate-400">Enable Dark Mode</span>
                         <DarkModeSwitch />
                     </div>
                 </section>
@@ -107,14 +108,14 @@ useEffect(()=>{
                     {isEditing ? (
                         <button
                             onClick={() => changeProfile()}
-                            className="bg-green-500 text-white px-6 py-2 rounded-lg shadow hover:bg-green-600 transition"
+                            className="bg-green-500 text-white px-6 py-2 cursor-pointer rounded-lg shadow hover:bg-green-600 transition"
                         >
                             Save Changes
                         </button>
                     ) : (
                         <button
                             onClick={() => setIsEditing(!isEditing)}
-                            className="bg-green-500 text-white px-6 py-2 rounded-lg shadow hover:bg-green-600 transition"
+                            className="bg-green-500 text-white px-6 py-2 cursor-pointer rounded-lg shadow hover:bg-green-600 transition"
                         >
                             Edit Profile
                         </button>
